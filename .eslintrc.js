63590105f9c55da19652df3808ecfea892de7ec5
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-07 19:47:00 +0100

    feat(tools): Make eslint stricted and run it before commits

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,24 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:vue/recommended",
+    "@vue/prettier",
+    "@vue/typescript"
+  ],
+  plugins: ["@typescript-eslint"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: "@typescript-eslint/parser",
+    project: "tsconfig.json"
   }
 };
