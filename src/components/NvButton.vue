9697cba165ec049006540b3540523f5acdb37e5d
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-08 21:06:59 +0100

    refactor(components): Break inputs and button to a seperate component

---
+++ src/components/NvButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="btn" @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "NvButton",
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  padding: 0.2rem 0.7rem;
+}
+</style>
